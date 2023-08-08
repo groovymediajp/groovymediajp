04468f46a449793cc93ba6bdf591930e50939e8f
Shota Watanabe <shota.w@gmail.com>
2023-08-08 18:06:20 +0900

    revalidate

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -4,7 +4,7 @@ import {client} from '../../../libs/client';
 
 import Post from '../../../components/article/Post';
 
-export const revalidate = 60;
+export const revalidate = 1;
 
 export default async function NewsDetail({ params }) {
   const data = await getData(params.slug);
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import HomeNews from '../components/home/HomeNews';
 
 import { client } from '../libs/client';
 
-export const revalidate = 60;
+export const revalidate = 1;
 
 export default async function Home() {
   const data = await getData();
